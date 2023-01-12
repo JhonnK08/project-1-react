0e175e7a1e5ee15719549a7737c3743ae0af2ddb
Jhonatan <38366068+JhonnK08@users.noreply.github.com>
2023-01-11 23:05:01 -0300

    feat: added test with mocks on Home

--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -1,5 +1,57 @@
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { Home } from '.';
+
+const handlers = [
+  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title1',
+          url: 'img1.jpg',
+          body: 'body1',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title2',
+          url: 'img2.jpg',
+          body: 'body2',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title3',
+          url: 'img3.jpg',
+          body: 'body3',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-  it('is a dummy test', () => {
-    expect(1).toBe(1);
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterEach(() => {
+    server.resetHandlers();
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+  it('is should render search, posts and load more', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts.');
+
+    await waitForElementToBeRemoved(noMorePosts);
+    screen.debug();
   });
 });
